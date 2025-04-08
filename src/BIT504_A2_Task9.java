cd8bb4f1f9361d6ccc6b931417de5f0d40787099
hjwebcraft <hjwebcraft@gmail.com>
2025-04-08 21:30:23 +1200

    This is the first change to my main code

--- src/BIT504_A2_Task9.java
+++ src/BIT504_A2_Task9.java
@@ -3,6 +3,7 @@ public class BIT504_A2_Task9 {
 
 	public static void main(String[] args) {
 		System.out.println("This is a simple project for demontrating GitHub basic functions");
+		System.out.println("This is the first feature change added to my project");
 
 	}
 

c498314b91a9fee01f1f1d0408f9c8d8bdb9b59b
hjwebcraft <hjwebcraft@gmail.com>
2025-04-08 21:34:26 +1200

    second branch change to then merge

--- src/BIT504_A2_Task9.java
+++ src/BIT504_A2_Task9.java
@@ -3,6 +3,8 @@ public class BIT504_A2_Task9 {
 
 	public static void main(String[] args) {
 		System.out.println("This is a simple project for demontrating GitHub basic functions");
+		System.out.println("This is a a 2nd Branch to my project with another addition to the code, after this i will merge the 2 branches to the main");
+		
 
 	}
 
